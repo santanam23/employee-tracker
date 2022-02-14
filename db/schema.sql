3856096cfd8ba6ecb28a1e4e151c05c10254454b
santanam23 <mirmir11.ms@gmail.com>
2022-02-14 15:12:01 -0600

    created employee table db

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ DROP TABLE IF EXISTS employee;
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    description TEXT
 );
 
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
+    salary DECIMAL NOT NULL,
     department_id INTEGER,
+    INDEX dep_index (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL   
 );
 
@@ -21,6 +21,9 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
+    INDEX role_index (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     manager_id INTEGER
+    INDEX manager_index (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
-
