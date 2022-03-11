cdac80e1947031ea159e96f800e96b14db4507d1
santanam23 <mirmir11.ms@gmail.com>
2022-03-11 13:25:21 -0600

    created employee add route

--- server.js
+++ server.js
@@ -61,22 +61,6 @@ app.get('/api/employee/:id', (req, res) => {
 });
 
 
-// Create a employee
-const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-              VALUES (?,?,?,?)`;
-const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
-
-db.query(sql, params, (err, result) => {
-  if (err) {
-    res.status(400).json({ error: err.message});
-    return;
-  } 
-  res.json({
-    message: 'success',
-    data: body
-  });
-});
-
 // Delete a employee
 app.delete('/api/employee/:id', (req, res) => {
   const sql = `DELETE FROM employee WHERE id = ?`;
@@ -105,6 +89,21 @@ app.post('/api/employee', ({body}, res) => {
     res.status(400).json({error: errors });
     return;
   }
+    // Create a employee
+  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+  VALUES (?,?,?,?)`;
+  const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+
+  db.query(sql, params, (err, result) => {
+  if (err) {
+  res.status(400).json({ error: err.message});
+  return;
+  } 
+  res.json({
+  message: 'success',
+  data: body
+  });
+ });
 });
 // Default response for any other request (Not Found)
 app.use((req, res) => {
