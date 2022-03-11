99f8bff7315fae27ff0b1d4d83651195402d0a2c
santanam23 <mirmir11.ms@gmail.com>
2022-03-11 12:54:54 -0600

    creating get route

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ron', 'Swanson', 1, 101),
+  ('Victoria', 'Justince', 1, 102),
+  ('Jack', 'Black', 2, 102),
+  ('Charles', 'Barkley', 3, 103),
+  ('Kevin', 'Hart', 1, 101),
+  ('Dora', 'Explorer', 2, 102),
+  ('Edward', 'Bel', 3, 103),
+  ('Monty', 'Summers', 1, 101),
+  ('Octavia', 'Butler', 2, 103),
+  ('Unica', 'Zurn', 3, 101);
--- server.js
+++ server.js
@@ -1,27 +1,62 @@
 const express = require('express');
-const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
-
-const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2');
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'R@m!r3Z*1',
+    database: 'employee_db'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM employee`, (err, rows) => {
+  console.log(rows);
+});
+
+// GET a single candidate
+db.query(`SELECT * FROM employee WHERE id = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
+});
+
+// Create a candidate
+// const sql = `INSERT INTO employee (id, first_name, last_name, role_id, manager_id) 
+//               VALUES (?,?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1, 102];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM employee WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-// Start server after DB connection
-db.connect(err => {
-  if (err) throw err;
-  console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
