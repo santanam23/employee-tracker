9f707d972f013b8372787ebf972dcd768c89559c
santanam23 <mirmir11.ms@gmail.com>
2022-03-11 19:39:14 -0600

    created employee db

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,19 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(50) NOT NULL,
     description TEXT
 );
 
@@ -9,13 +21,6 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INTEGER
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
-
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER
-)
--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,20 @@ VALUES
   ('Monty', 'Summers', 1, 101),
   ('Octavia', 'Butler', 2, 103),
   ('Unica', 'Zurn', 3, 101);
+
+INSERT INTO department (name, description)
+VALUES 
+    ('Sales', 'Focuses on making sure the department is making its sales quota throughout the year'),
+    ('Engineering', 'Focuses on making sure our systems are running in place and we do not have any unexpected issues'),
+    ('Finance', 'Focuses on making sure we are spending within our budget'),
+    ('Legal', 'Focuses on legal matters and makes sure our contracts are beneficial for the company')
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Sales', 80000, 1),
+  ('Engineering', 150000, 2),
+  ('Engineering', 120000, 2),
+  ('Finance', 160000, 3),
+  ('Finance', 125000, 3),
+  ('Legal', 250000, 4),
+  ('Legal', 190000, 4);
