79caf66cbd599213583a4818d706072fba16273a
santanam23 <mirmir11.ms@gmail.com>
2022-03-16 12:27:36 -0500

    fixing constraint

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,6 @@
-DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
-
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
-);
+DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -23,4 +14,13 @@ CREATE TABLE role (
     salary DECIMAL,
     department_id INTEGER,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL   
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,3 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES
-  ('Ron', 'Swanson', 1, NULL),
-  ('Victoria', 'Justince', 2, 2),
-  ('Jack', 'Black', 3, 3),
-  ('Charles', 'Barkley', 4, NULL),
-  ('Kevin', 'Hart', 5, 5),
-  ('Dora', 'Explorer', 6, NULL),
-  ('Edward', 'Bel', 7, 7),
-  ('Monty', 'Summers', 8, NULL),
-  ('Octavia', 'Butler', 8, NULL),
-  ('Unica', 'Zurn', 9, NULL);
 
 INSERT INTO department (name, description)
 VALUES 
@@ -27,3 +15,16 @@ VALUES
   ('Finance', 125000, 3),
   ('Legal', 250000, 4),
   ('Legal', 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Ron', 'Swanson', 1, NULL),
+  ('Victoria', 'Justince', 2, 2),
+  ('Jack', 'Black', 3, 3),
+  ('Charles', 'Barkley', 4, NULL),
+  ('Kevin', 'Hart', 5, 5),
+  ('Dora', 'Explorer', 6, NULL),
+  ('Edward', 'Bel', 7, 7),
+  ('Monty', 'Summers', 8, NULL),
+  ('Octavia', 'Butler', 8, NULL),
+  ('Unica', 'Zurn', 9, NULL);
