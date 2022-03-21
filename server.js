ae01c8b9f6c9d426b4061c0f4d536ad9aa581077
santanam23 <mirmir11.ms@gmail.com>
2022-03-21 11:58:56 -0500

    created view and add section

--- server.js
+++ server.js
@@ -100,11 +100,7 @@ function start() {
       });
     }
   function viewAllEmployees(){
-    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, 
-    CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee
-    LEFT JOIN role ON employee.role_id = role.id
-    LEFT JOIN department ON role.department_id = department.id
-    LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+    const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name`;
     db.query(sql, (err, rows) => {
       if (err) throw err;
       console.table(rows);
